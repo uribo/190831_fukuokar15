55c5005507e221702c0a6dd0a2313ff83695ee2c
Shinya Uryu <suika1127@gmail.com>
2019-08-30 06:59:57 +0900

    Update

--- .Rprofile
+++ .Rprofile
@@ -1,1 +1,7 @@
+options(
+reprex.highlight.hl_style  = "base16/github",
+reprex.highlight.font      = "Fira Code",
+reprex.highlight.font_size = 36
+)
+invisible(source("https://gist.githubusercontent.com/uribo/bc5df991469c8024cc3db78aa669df7a/raw/46531b306713fadb268ec1559a00a46891b67aaf/ds_view.R"))
 source("renv/activate.R")
--- tidymodels_flow.R
+++ tidymodels_flow.R
@@ -43,3 +43,38 @@ plan_iris_classification <-
       bind_cols(iris_testing))
 make(plan_iris_classification)
 loadd(plan_iris_classification, list = plan_iris_classification$target)
+
+
+iris_ranger %>%
+  predict(iris_testing) %>%
+  bind_cols(iris_testing) %>%
+  metrics(truth = Species, estimate = .pred_class)
+
+perf_metrics <- metric_set(accuracy, kap)
+iris_ranger %>%
+  predict(iris_testing) %>%
+  bind_cols(iris_testing) %>%
+  perf_metrics(truth = Species, estimate = .pred_class)
+
+iris_probs %>%
+  gain_curve(Species, .pred_setosa:.pred_virginica) %>%
+  glimpse()
+
+iris_probs %>%
+  gain_curve(Species, .pred_setosa:.pred_virginica) %>%
+  autoplot()
+
+iris_probs %>%
+  roc_curve(Species, .pred_setosa:.pred_virginica) %>%
+  autoplot()
+
+predict(iris_ranger, iris_testing, type = "prob") %>%
+  bind_cols(predict(iris_ranger, iris_testing)) %>%
+  bind_cols(select(iris_testing, Species)) %>%
+  metrics(Species, .pred_setosa:.pred_virginica, estimate = .pred_class)
+
+iris_rf %>%
+  predict(iris_testing) %>%
+  bind_cols(iris_testing) %>%
+  metrics(truth = Species, estimate = .pred_class)
+
